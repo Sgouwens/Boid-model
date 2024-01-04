e5787b3dd32c7fecab19a8a7a47d023ef18990f5
Sigur Gouwens <sigur.gouwens@rivm.nl>
2024-01-04 11:03:32 +0100

    Fixed bug in cohesion step. updates to the pygame. fixed issue where all boids turned to same direction

--- boid_rules.py
+++ boid_rules.py
@@ -1,50 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial.transform import Rotation
-from numba import jit
 import time
 
-# Om een of andere reden zorgt cohesion rate dat er rondjeso m een willekeurig punt gedraaid worden.
+from scipy.spatial.transform import Rotation
+from numpy.linalg import norm
 
 class Boids():
     """This class will be a group of Boids, consisting of both preditors and preys"""
     
     def __init__(self, num_boids=10, n_dim=2, timestep=1):
-        self.positions = np.random.uniform(-150,150, n_dim*num_boids).reshape(num_boids, n_dim)
+        
+        # Testing starting conditions
+        # self.positions = np.hstack((np.linspace(-50,50, num_boids).reshape(-1,1),
+        #                             np.zeros(num_boids).reshape(-1,1)))
+        # self.velocities = np.hstack((np.zeros(num_boids).reshape(-1,1),
+        #                             np.ones(num_boids).reshape(-1,1)))
+        
+        # Random starting conditions
+        self.positions = np.random.uniform(-30,30, n_dim*num_boids).reshape(num_boids, n_dim)
         self.velocities = np.random.normal(0, 0.1, n_dim*num_boids).reshape(num_boids, n_dim)
-        self.velocities /= np.linalg.norm(self.velocities, axis=0, keepdims=True)
+        self.velocities /= norm(self.velocities, axis=0, keepdims=True)
         
         self.num_boids = num_boids
         self.n_dim = n_dim
         self.timestep = timestep
                 
-    @staticmethod
-    def flock_mean(arr):
-        """Computes the center of the group as well as the group's direction"""
-        mean = np.mean(arr, axis=0)
-        return mean
+    # @staticmethod
+    # def flock_mean(arr):
+    #     """Computes the center of the group as well as the group's direction"""
+    #     mean = np.mean(arr, axis=0)
+    #     return mean
     
-    #@jit
     def get_local_flock_idx(self, boid_idx, radius):
         """Within a flock of birds, we distinguish local groups. These are the 
         boids that respond to each other in terms of movement. This function
         finds the boid indices of the group a specific boid responds to.
         By convention, a boid is not in own group preventing interacting with itself"""
-        flock_idx = np.linalg.norm(self.positions - self.positions[boid_idx,:], axis=1) < radius        
+        flock_idx = norm(self.positions - self.positions[boid_idx,:], axis=1) < radius        
         flock_idx[boid_idx] = False
         
         return np.where(flock_idx)[0]
             
     @staticmethod
-    @jit
     def steer_away(vel_boid, vel_group, prop):
         
-        vel_boid_dir = vel_boid / np.linalg.norm(vel_boid)
-        vel_group_dir = vel_group / np.linalg.norm(vel_group).reshape(-1,1)
+        vel_boid_dir = vel_boid / norm(vel_boid)
+        vel_group_dir = vel_group / norm(vel_group).reshape(-1,1)
         
-        displacement = (prop.reshape(-1, 1)**1) * (vel_boid_dir - vel_group_dir) # squared
+        displacement = (prop.reshape(-1, 1)**2) * (vel_boid_dir - vel_group_dir) # squared
         vel_boid_update = vel_boid + np.mean(displacement, axis=0)
-        vel_boid_update /= np.linalg.norm(vel_boid_update)
+        vel_boid_update /= norm(vel_boid_update)
         
         return vel_boid_update
     
@@ -54,19 +59,22 @@ class Boids():
         Output: Size of velocity nudge"""
         
         group_vel = self.velocities[idx_group, :]
-        group_mean_dir = self.flock_mean(group_vel)
-        group_mean_dir /= np.linalg.norm(group_mean_dir)
+        group_mean_dir = np.mean(group_vel, axis=0)
+        group_mean_dir /= norm(group_mean_dir)
         
-        boid_dir = self.velocities[idx, :] / np.linalg.norm(self.velocities[idx, :])
+        boid_dir = self.velocities[idx, :] / norm(self.velocities[idx, :])
         
         boid_dir_updated = boid_dir + alpha * group_mean_dir   
-        boid_dir_updated /= np.linalg.norm(boid_dir_updated)
+        boid_dir_updated /= norm(boid_dir_updated)
         
         return boid_dir_updated - boid_dir
     
     def flock_separation(self, idx, alpha, idx_group):
         """Steer to avoid crowding local flockmates. If no flockmates are within the specified
         radius, the radius is increased"""
+        
+        # Change this such that rotation matrix is used. Current implementation can cause weird issues
+        # To do this, make a new function 'find_angle'
         
         group_pos = self.positions[idx_group, :]
         group_vel = self.velocities[idx_group, :]
@@ -77,14 +85,23 @@ class Boids():
         # Simple Euler forward approximation of next position
         group_pos_update = group_pos + self.timestep * group_vel
         
-        group_pos_distances = np.linalg.norm(boid_pos - group_pos_update, axis=1)
+        group_pos_distances = norm(boid_pos - group_pos_update, axis=1)
         group_prop_repulsion = 1 / group_pos_distances
         
         boid_vel = self.steer_away(boid_vel, group_vel, group_prop_repulsion) - boid_vel
         
         return alpha*boid_vel
     
-    #@jit
+    @staticmethod
+    def find_angle(vec1, vec2):
+        
+        clockwise = np.sign(np.linalg.det((vec1, vec2)))
+        dotproduct = np.dot(vec1, vec2)
+        denominator = norm(vec1) * norm(vec2)
+        angle = clockwise * np.arccos(dotproduct / denominator)
+        
+        return angle
+    
     def flock_cohesion(self, idx, alpha, idx_group=[], to_center=False):
         """steer to move towards the average position (center of mass) of local flockmates
         currently, the center point is still. 
@@ -92,29 +109,22 @@ class Boids():
         
         if len(idx_group)>0:
             if len(idx_group)>1:
-                group_pos = np.mean(self.positions[idx_group, :], axis=0)
+                group_pos_timestep = self.positions[idx_group, :] + self.timestep * self.velocities[idx_group, :]
+                group_pos = np.mean(group_pos_timestep, axis=0)
             else:
-                return 0 #group_pos = np.mean(self.positions[idx_group, :], axis=0)
+                return 0
         else:
             group_pos = np.array([0, 0])
             
-        boid_pos = self.positions[idx, :]
-        boid_vel = self.velocities[idx, :]
+        velocity = self.velocities[idx,:]
+        position = self.positions[idx,:]
+        boid_to_center = group_pos - position
         
-        # First find the angle between the boids position and direction and the centerpoint
-        boid_direction = boid_pos + boid_vel
-        boid_to_center = group_pos - boid_pos
-        
-        dotproduct = np.dot(boid_direction, boid_to_center)
-        denominator = np.linalg.norm(boid_direction) * np.linalg.norm(boid_to_center)
-        angle_to_center = np.arccos(dotproduct / denominator)
-        
-        # Reduce the angle by alpha and apply to velocity vector
-        angle_new = alpha*angle_to_center
+        angle_new = alpha * self.find_angle(velocity, boid_to_center)
         rotation_matrix = Rotation.from_euler('z', angle_new).as_matrix()[:2, :2]
-        rotated_vector = np.dot(rotation_matrix, boid_vel) 
+        rotated_vector = np.dot(rotation_matrix, velocity) 
         
-        return rotated_vector - boid_vel
+        return rotated_vector - velocity
         
     def flock_update(self, radius, c_rate, a_rate, s_rate):
         
@@ -134,21 +144,22 @@ class Boids():
             store_velocity[idx, :] += self.flock_separation(idx, alpha=s_rate, idx_group=flock_idx)
             store_velocity[idx, :] += self.flock_alignment(idx, alpha=a_rate, idx_group=flock_idx)
             store_velocity[idx, :] += self.flock_cohesion(idx, alpha=c_rate, idx_group=flock_idx)
-            store_velocity[idx, :] += self.flock_cohesion(idx, alpha=0.01)
+            store_velocity[idx, :] += self.flock_cohesion(idx, alpha=0.005)
         
         self.velocities += store_velocity
-        self.velocities /= np.linalg.norm(self.velocities, axis=1).reshape(-1, 1)
+        
+        # velocities = norm(self.velocities, axis=1).reshape(-1, 1)
+        # velocities_cor = 1 + np.tanh(velocities-1)
+        
+        self.velocities /= norm(self.velocities, axis=1).reshape(-1, 1)
+        # self.velocities *= velocities_cor
+        
         self.positions += store_position + 1*self.timestep * self.velocities
-
-if __name__ == "__main__":
-    
+        
+# This part is for checking computation speed
+t = time.perf_counter()
+if __name__ == "__main__":    
     flock = Boids(num_boids=60, n_dim=2, timestep=1)
-
-    for i in range(2000):
-        flock.flock_update(radius=10, c_rate=0.01, a_rate=0.01, s_rate=0.01, )
-        plt.quiver(flock.positions[:,0], flock.positions[:,1],
-                   flock.velocities[:,0], flock.velocities[:,1])
-        plt.xlim(-250, 250)
-        plt.ylim(-250, 250)
-        plt.show()
-    
+    for i in range(400):
+        flock.flock_update(radius=10, c_rate=0.01, a_rate=0.01, s_rate=0.01)
+print(time.perf_counter() - t)
--- boid_simulation.py
+++ boid_simulation.py
@@ -1,7 +1,15 @@
 from boid_rules import Boids
 import numpy as np
-
+import time
+import sys
 import pygame
+
+# TODO: 
+# 1) allow varying speeds among boids (contract towards 1 by updating |v| = np.tanh(|v|))
+# 2) try to apply the rotation trick of cohesion to other update rules
+# 3) include an euler forward step before calculating angle (done
+# 4) [FOCUS] separation does not appear to work. find out why.
+# 5) make cohesion proportional to distance. weak when close, strong when far.
 
 def print_parameters(c, a, s, r):
     print(f"Cohesion: {c:.2f}, Alignment: {a:.2f}, Separation: {s:.2f}, Radius: {r}, ")
@@ -11,15 +19,16 @@ width, height = 500, 500
 screen = pygame.display.set_mode((width, height))
 
 # Set up colors
-white = (0, 0, 255)
-blue = (255, 255, 255)
-red = (255, 0, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+lightblue = (0, 0, 200)
+white = (255,255,255)
 
 arrow_length = 5
 
 # Initialise the flock
-flocksize = 150
-radius = 10
+flocksize = 100
+radius = 50
 flock = Boids(num_boids=flocksize, n_dim=2, timestep=1)
 
 # Configurations
@@ -31,6 +40,7 @@ separation_rate = 0.01
 clock = pygame.time.Clock() 
 running = True
 while running:
+    #time.sleep(1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -63,12 +73,15 @@ while running:
 
     flock.flock_update(radius=10, c_rate=cohesion_rate, a_rate=alignment_rate, s_rate=separation_rate)
 
-    screen.fill(white)
+    screen.fill(blue)
+    
+    pygame.draw.circle(screen, black, [width/2, width/2], radius=radius/2)
+    pygame.draw.circle(screen, blue, [width/2, width/2], radius=radius/2-1)
 
     for idx in range(flocksize):
         arrow_end = [flock.positions[idx, 0] + arrow_length * flock.velocities[idx, 0] + width/2,
                       flock.positions[idx, 1] + arrow_length * flock.velocities[idx, 1] + width/2]
-        pygame.draw.line(screen, blue, flock.positions[idx, :] + np.array([width/2, width/2]), arrow_end, 2)
+        pygame.draw.line(screen, white, flock.positions[idx, :] + np.array([width/2, width/2]), arrow_end, 2)
 
     pygame.display.flip()
     clock.tick(30)
