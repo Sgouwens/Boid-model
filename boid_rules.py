ad334b1ba37047730e7d7bc41597d08b8d5d4b97
Sigur Gouwens <sigur.gouwens@rivm.nl>
2024-01-09 16:20:57 +0100

    Model works fully angular. Attraction on mouse click. bugfixes

--- boid_rules.py
+++ boid_rules.py
@@ -6,30 +6,27 @@ from scipy.spatial.transform import Rotation
 from numpy.linalg import norm
 
 class Boids():
-    """This class will be a group of Boids, consisting of both preditors and preys"""
+    """This class will be a group of Bird-oid objects (Boids), 
+    consisting of both preditors and preys"""
     
     def __init__(self, num_boids=10, n_dim=2, timestep=1):
         """Only the number of boids and dimensions are needed. Currently only n_dim=2 
         is supported. Leave unchanged for more natural starting conditions."""
         self.positions = np.random.uniform(-20,20, n_dim*num_boids).reshape(num_boids, n_dim)
-        self.velocities = np.full(num_boids, 1).reshape(-1,1)
-        self.angles = np.random.uniform(0, 2*np.pi, num_boids).reshape(-1,1)
-        
+        self.velocities = np.random.normal(1, 0.05, num_boids).reshape(-1, 1)
+        self.angles = np.random.uniform(0, 2*np.pi, num_boids).reshape(-1, 1)
+        self.angles = np.full(num_boids, 2*np.pi/4).reshape(-1, 1)
         self.num_boids = num_boids
         self.n_dim = n_dim
         self.timestep = timestep
         
-    def add_noise_velocity(sd):
-        return np.random.normal(0, sd, self.num_boids) 
+    def add_noise_velocity(self, sd):
+        return np.random.normal(0, sd, self.num_boids).reshape(-1, 1)
     
     def polar_to_vec(self, velocities, angles):   
-        return np.hstack((np.sin(angles), np.cos(angles)))#*velocities
+        return velocities * np.hstack((np.sin(angles), np.cos(angles)))
     
     def next_position(self, positions, velocities, angles):
-        # The reshapes inside this function do not behave as wantes
-        # voor flock_cohesion moeten we vel en ang reshape(-1.1).... bij align juist niet?
-        print('positions', positions)
-        print('direction', self.polar_to_vec(velocities, angles))
         return positions + self.timestep * self.polar_to_vec(velocities, angles)
        
     def get_local_flock_idx(self, boid_idx, radius):
@@ -37,25 +34,25 @@ class Boids():
         boids that respond to each other in terms of movement. This function
         finds the boid indices of the group a specific boid responds to.
         By convention, a boid is not in own group preventing interacting with itself"""
-        flock_idx = norm(self.positions - self.positions[boid_idx,:], axis=1) < radius        
+        
+        flock_idx = norm(self.positions - self.positions[boid_idx,:], axis=1) < radius 
         flock_idx[boid_idx] = False
-                
-        return np.where(flock_idx)[0]
+        patat = np.where(flock_idx)[0]
+        return patat
         
     def flock_alignment(self, idx, alpha, idx_group):
         """Steer towards the average heading of local flockmates with rate alpha
         Input: boid, flock of boid, effect strength
         Output: Size of velocity nudge towards average flock direction"""
         boid_angle = self.angles[idx]
-        group_mean_angle = np.mean(self.angles[idx_group.reshape(-1,1)])
+        group_mean_angle = np.mean(self.angles[idx_group])
         
         angle_effect = alpha * (boid_angle - group_mean_angle)
         return angle_effect
         
-    def flock_separation(self, idx, alpha, idx_group):#################################
+    def flock_separation(self, idx, alpha, idx_group):
         """Steer to avoid crowding local flockmates. If no flockmates are within the specified
         radius, the radius is increased"""
-        
         group_pos = self.positions[idx_group, :]
         group_vel = self.velocities[idx_group]
         group_ang = self.angles[idx_group]
@@ -68,34 +65,29 @@ class Boids():
         group_pos_update = self.next_position(group_pos, group_vel, group_ang)
         boid_pos_update = self.next_position(boid_pos, boid_vel, boid_ang)
         
-        print(boid_pos_update)
-        
         repulsion = norm(boid_pos_update - group_pos_update, axis=1) ** (-2)
-        repulsion /= np.abs(repulsion)
-        
-        angle_change = repulsion * group_ang
-        print(group_ang)
+        repulsion /= np.sum(np.abs(repulsion))
+        angle_change = np.dot(repulsion, group_ang)
         
         return alpha * angle_change
         
-    def flock_cohesion(self, idx, alpha, idx_group=[], to_center=False):
+    def flock_cohesion(self, idx, alpha, idx_group=[], centerpoint=None):
         """steer to move towards the average position (center of mass) of local flockmates
         currently, the center point is still. 
         We can choose to first move the center point one timestep forward"""
         if len(idx_group)>0:
-            #return 1
             group_pos = self.next_position(self.positions[idx_group,:], 
-                                            self.velocities[idx_group], 
-                                            self.angles[idx_group])
+                                           self.velocities[idx_group], 
+                                           self.angles[idx_group])
             group_pos = np.mean(group_pos, axis=0)
         else:
-            group_pos = np.array([0, 0])
+            group_pos = centerpoint
             
         position = self.positions[idx,:]
         velocity = self.velocities[idx]
         angle = self.angles[idx]
         
-        position_upd = self.next_position(position, velocity, angle)[0]
+        position_upd = self.next_position(position, velocity, angle)
         
         boid_dir = position_upd - position
         boid_to_center = group_pos - position
@@ -108,7 +100,7 @@ class Boids():
         
         return alpha * sign * angle
         
-    def flock_update(self, radius, c_rate, a_rate, s_rate, contraction=False):
+    def flock_update(self, radius, c_rate, a_rate, s_rate, o_rate, center=np.array([0,0])):
         """Determines the flock for each boid, then computes the effects of alignment, cohesion
         and separation. Optionally control for speed by using the tanh contraction function."""
         store_angles = np.full(self.num_boids, 0, dtype=np.float64).reshape(-1, 1)
@@ -120,12 +112,12 @@ class Boids():
             while not np.any(flock_idx):
                 flock_idx = self.get_local_flock_idx(idx, radius_local)
                 radius_local *= 1.1
-                                            
-            # store_angles[idx] += self.flock_separation(idx, alpha=s_rate, idx_group=flock_idx)
+                
+            store_angles[idx] -= self.flock_separation(idx, alpha=s_rate, idx_group=flock_idx)
             store_angles[idx] -= self.flock_alignment(idx, alpha=a_rate, idx_group=flock_idx)
             store_angles[idx] -= self.flock_cohesion(idx, alpha=c_rate, idx_group=flock_idx)
-            store_angles[idx] -= self.flock_cohesion(idx, alpha=0.01)
-            #store_angles += self.add_noise_velocity(0.1)
+            store_angles[idx] -= self.flock_cohesion(idx, alpha=o_rate, centerpoint=center)
+            store_angles += self.add_noise_velocity(0.000)
             
         self.angles += store_angles
         self.positions = self.next_position(self.positions, self.velocities, self.angles)
@@ -135,9 +127,10 @@ class Boids():
 # This part is for checking computation speed
 if __name__ == "__main__":    
     t = time.perf_counter()
-    flock = Boids(num_boids=4, n_dim=2, timestep=1)
-    for i in range(3):
-        flock.flock_update(radius=10, c_rate=0.05, a_rate=0.01, s_rate=0.01)
+    flock = Boids(num_boids=10, n_dim=2, timestep=1)
+    for i in range(2):
+        print(i)
+        flock.flock_update(radius=10, c_rate=0.05, a_rate=0.05, s_rate=0.01, o_rate=0.01)
         plt.quiver(flock.positions[:,1], flock.positions[:,0],
                     flock.velocities*np.cos(flock.angles), flock.velocities*np.sin(flock.angles))
     #print(time.perf_counter() - t)
--- boid_simulation.py
+++ boid_simulation.py
@@ -5,30 +5,30 @@ import sys
 import pygame
 
 # TODO: 
-# 2) try to apply the rotation trick of cohesion to other update rules
-# 3) include an euler forward step before calculating angle (done
-# 4) [FOCUS] separation does not appear to work. find out why.
-# 6) when finding angle between vectors, values outside [-1,1] are given as input to arccos -> crash
-# 7) add a predator boid from which the regular boids free (idea: use alignment such that 
+# 9) add a predator boid from which the regular boids free (idea: use alignment such that 
 #    the the boids update to the velocity perpendicular to the predator velocity)
 
 def print_parameters(c, a, s, r):
     print(f"Cohesion: {c:.2f}, Alignment: {a:.2f}, Separation: {s:.2f}, Radius: {r}, ")
 
 pygame.init()
-width, height = 500, 500
-screen = pygame.display.set_mode((width, height))
+size = 500
+screen = pygame.display.set_mode((size, size))
 
 # Set up colors
 blue = (0, 0, 255)
+red = (255, 0, 0)
 black = (0, 0, 0)
 lightblue = (0, 0, 200)
 white = (255,255,255)
 
-arrow_length = 5
+mouse_x, mouse_y = size/2, size/2
+new_center = np.array([0, 0])
+
+arrow_length =  5
 
 # Initialise the flock
-flocksize = 250
+flocksize = 100
 radius = 50
 flock = Boids(num_boids=flocksize, n_dim=2, timestep=1)
 
@@ -36,12 +36,13 @@ flock = Boids(num_boids=flocksize, n_dim=2, timestep=1)
 cohesion_rate = 0.01
 alignment_rate = 0.01
 separation_rate = 0.01
+origin_rate = 0.01
 
 # Pygame loop
 clock = pygame.time.Clock() 
 running = True
 while running:
-    #time.sleep(1)
+    #time.sleep(0.1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -49,6 +50,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 flock = Boids(num_boids=flocksize, n_dim=2, timestep=1); print('Flock reset')
+                new_center = np.array([0, 0])
             
             if event.key == pygame.K_UP:
                 cohesion_rate += 0.01
@@ -69,23 +71,33 @@ while running:
                 radius -= 1
             if event.key == pygame.K_s:
                 radius += 1
-            
             print_parameters(cohesion_rate, alignment_rate, separation_rate, radius)
-
-    flock.flock_update(radius=30, c_rate=cohesion_rate, a_rate=alignment_rate, s_rate=separation_rate)
+            
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Left mouse button
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                new_center = np.array([mouse_x-size/2, mouse_y-size/2])
+                print(f"Centerpoint updated to: ({mouse_x}, {mouse_y})")
+            print_parameters(cohesion_rate, alignment_rate, separation_rate, radius)
+            
+    flock.flock_update(radius=20, 
+                       c_rate=cohesion_rate, 
+                       a_rate=alignment_rate, 
+                       s_rate=separation_rate,
+                       o_rate=origin_rate,
+                       center=new_center)
 
     screen.fill(blue)
-    
-    pygame.draw.circle(screen, black, [width/2, width/2], radius=radius/2)
-    pygame.draw.circle(screen, blue, [width/2, width/2], radius=radius/2-1)
+    pygame.draw.circle(screen, black, [size/2, size/2], radius=radius/2)
+    pygame.draw.circle(screen, blue, [size/2, size/2], radius=radius/2-1)
+    pygame.draw.circle(screen, red, [mouse_x, mouse_y], radius=2)
 
     for idx in range(flocksize):
         
         arrows = flock.polar_to_vec(flock.velocities, flock.angles)
-        666
-        arrow_end = [flock.positions[idx, 0] + arrow_length * arrows[idx, 0] + width/2,
-                      flock.positions[idx, 1] + arrow_length * arrows[idx, 1] + width/2]
-        pygame.draw.line(screen, white, flock.positions[idx, :] + np.array([width/2, width/2]), arrow_end, 2)
+        arrow_end = [flock.positions[idx, 0] + arrow_length * arrows[idx, 0] + size/2,
+                      flock.positions[idx, 1] + arrow_length * arrows[idx, 1] + size/2]
+        pygame.draw.line(screen, white, flock.positions[idx, :] + np.array([size/2, size/2]), arrow_end, 2)
 
     pygame.display.flip()
     clock.tick(30)
